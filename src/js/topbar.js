7205bda9b7b2ea14690d08b73e6380e4f335b889
Calvin Lee <calvinnleeee@gmail.com>
2024-12-16 13:58:08 -0800

    Added test background images, start on top bar.

---
+++ src/js/topbar.js
@@ -0,0 +1,20 @@
+/*
+  Create the top navbar to hold links
+ */
+
+function topbar() {
+  content_div = document.getElementById("content");
+
+  // create main div
+  const div = document.createElement("div");
+  div.classList.add("container h-48 flex")
+
+  // name
+  const name = document.createElement("h1");
+  name.classList.add("text-2xl");
+  name.innerText = "{calvin.lee}";
+
+  const about = document.createElement("a");
+  about.classList.add("")
+}
+topbar()
