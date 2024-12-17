6e2d67a7e5aede5229b885fd4975a56ed871f866
Calvin Lee <calvinnleeee@gmail.com>
2024-12-17 13:52:15 -0800

    Updated about section.
    Added short intro with some key points about myself.
    Tested using VS Code Live Server to read JSON data and load it into the page.
    Formatting of page and js files updated.

---
+++ src/js/about.js
@@ -0,0 +1,58 @@
+/*
+  'About' content, introducing myself and maybe some interesting tidbits.
+*/
+function about() {
+  // create the main div
+  const about_content = document.createElement("div");
+  about_content.classList.add(
+    "container", "h-max", "w-full", "flex", "flex-col", "mt-5"
+  );
+  about_content.id = "current"
+
+  // spacer
+  let spacer = document.createElement("div");
+  spacer.classList.add("h-20");
+
+  // top-level intro
+  let name = document.createElement("h2");
+  name.classList.add("text-4xl", "font-bold", "dark:text-white", "text-left", 'mb-6');
+  name.innerText = "Hi, I'm Calvin :) I am...";
+
+  ////////////////////////////////////////////////////////
+  // intro list
+  let intro = document.createElement("ul");
+  intro.classList.add("list-none", "ml-16");
+
+  fetch("./src/json/intro.json").then(res => res.json()).then(data => {
+    for (let idx in data) {
+      let point = data[idx]
+      let list_item = document.createElement("li");
+      list_item.classList.add("text-3xl", "dark:text-white", "text-left");
+      list_item.innerText = point["main"];
+  
+      let list_desc = document.createElement("ul");
+      list_desc.classList.add("text-xl", "dark:text-white", "text-left", "ml-12", "my-2", "list-disc");
+      for (let idx in point["desc"]) {
+        let dsc = point["desc"][idx]
+        let item = document.createElement("li");
+        item.classList.add("mb-2");
+        item.innerText = dsc;
+        list_desc.appendChild(item);
+      }
+      list_item.appendChild(list_desc);
+      intro.appendChild(list_item);
+    }
+  })
+  
+
+
+  ////////////////////////////////////////////////////////
+  // add content to the div
+  about_content.appendChild(spacer);
+  about_content.appendChild(name);
+  about_content.appendChild(intro);
+
+  // add to main div
+  document.getElementById("content").appendChild(about_content);
+};
+about();
--- src/js/topbar.js
+++ src/js/topbar.js
@@ -1,41 +1,48 @@
 /*
-  Create the top navbar to hold links
- */
-
+  Create the top 'navbar'.
+*/
 function topbar() {
-  const content_div = document.getElementById("content");
-
   // create main div
   let div = document.createElement("div");
   div.classList.add("container", "w-full", "h-36", "flex", "items-center", "mt-5", "fade-in");
 
   // name
   let name = document.createElement("h1");
-  name.classList.add("h-fit", "w-max" ,"text-4xl", "text-left", "dark:text-white", "basis-2/5");
-  name.innerText = "{calvin.lee}";
+  name.classList.add("h-fit", "w-max", "text-4xl", "text-left", "dark:text-white", "basis-2/5");
+  name.innerText = "{ calvin.lee }";
 
   // about
   let about = document.createElement("p");
-  about.classList.add("h-fit", "w-max" ,"text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
-  about.innerText = "About me";
+  about.classList.add("h-fit", "w-max", "text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
+  about.innerText = "About";
+  about.addEventListener("click", function (e) {
+    // navigate to home
+  });
 
   // portfolio
   let portfolio = document.createElement("p");
-  portfolio.classList.add("h-fit", "w-max" ,"text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
+  portfolio.classList.add("h-fit", "w-max", "text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
   portfolio.innerText = "Portfolio";
+  portfolio.addEventListener("click", function (e) {
+    // navigate to skills/experiences
+  });
 
-  // links
-  let links = document.createElement("p");
-  links.classList.add("h-fit", "w-max" ,"text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
-  links.innerText = "Links";
+  // contact
+  let contact = document.createElement("p");
+  contact.classList.add("h-fit", "w-max", "text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
+  contact.innerText = "Contact";
+  contact.addEventListener("click", function (e) {
+    // navigate to contact links
+  });
 
+  ////////////////////////////////////////////////////////
   // add to navbar
   div.appendChild(name);
   div.appendChild(about);
   div.appendChild(portfolio);
-  div.appendChild(links);
+  div.appendChild(contact);
 
   // display to screen
-  content_div.appendChild(div);
+  document.getElementById("content").appendChild(div);
 }
 topbar();
