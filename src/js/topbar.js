e7a4f709a62b7b6c5faa02bc30121a62a6284986
Calvin Lee <calvinnleeee@gmail.com>
2024-12-16 23:57:43 -0800

    Basic top bar implementation.
    Added name and titles of each section to the header, prepared for linking to new pages once the content for those pages is complete.

--- src/js/topbar.js
+++ src/js/topbar.js
@@ -3,18 +3,39 @@
  */
 
 function topbar() {
-  content_div = document.getElementById("content");
+  const content_div = document.getElementById("content");
 
   // create main div
-  const div = document.createElement("div");
-  div.classList.add("container h-48 flex")
+  let div = document.createElement("div");
+  div.classList.add("container", "w-full", "h-36", "flex", "items-center", "mt-5", "fade-in");
 
   // name
-  const name = document.createElement("h1");
-  name.classList.add("text-2xl");
+  let name = document.createElement("h1");
+  name.classList.add("h-fit", "w-max" ,"text-4xl", "text-left", "dark:text-white", "basis-2/5");
   name.innerText = "{calvin.lee}";
 
-  const about = document.createElement("a");
-  about.classList.add("")
+  // about
+  let about = document.createElement("p");
+  about.classList.add("h-fit", "w-max" ,"text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
+  about.innerText = "About me";
+
+  // portfolio
+  let portfolio = document.createElement("p");
+  portfolio.classList.add("h-fit", "w-max" ,"text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
+  portfolio.innerText = "Portfolio";
+
+  // links
+  let links = document.createElement("p");
+  links.classList.add("h-fit", "w-max" ,"text-2xl", "dark:text-white", "cursor-pointer", "basis-1/5");
+  links.innerText = "Links";
+
+  // add to navbar
+  div.appendChild(name);
+  div.appendChild(about);
+  div.appendChild(portfolio);
+  div.appendChild(links);
+
+  // display to screen
+  content_div.appendChild(div);
 }
-topbar()
+topbar();
