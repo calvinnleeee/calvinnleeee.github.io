0d8e35c0cd1e3c8dd9355ae4fdf92210b82d5206
Calvin Lee <calvinnleeee@gmail.com>
2026-01-15 11:18:11 -0800

    Update tailwind config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "*.html"],
+  content: ["./src/*.{js,css}", "./src/js/*.{html,js}", "index.html"],
   theme: {
     extend: {},
   },
